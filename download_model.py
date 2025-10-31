fab9fd354e782c5b8f8a7a2a3f8bf510e6ebeb92
Kavya Sri <kavyasrirudrakshala@gmail.com>
2025-10-31 09:30:43 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -2,14 +2,15 @@
 import os
 import gdown
 
-url = "https://drive.google.com/uc?id=1bquBi_ccK4XDsatiHZsucysPUBXzmga6"
+url = "https://drive.google.com/uc?export=download&id=1Yy3ItSh6tFbGN75_Vd00kX068GN9jESz"
 
 
-output = "models/phi-2.Q4_K_M.gguf"
+output = "models/tinyllama-1.1b-chat.Q4_K_M.gguf"
 
 os.makedirs(os.path.dirname(output), exist_ok=True)
 
-print("Downloading model file from Google Drive...")
+print("Downloading TinyLlama model file from Google Drive...")
 gdown.download(url, output, quiet=False)
 print(f"✅ Model saved to {output}")
+
 
