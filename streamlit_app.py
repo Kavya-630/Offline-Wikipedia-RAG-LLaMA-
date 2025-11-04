f2d029a2559db80d4e149b001746c9f139ee6515
Kavya Sri <kavyasrirudrakshala@gmail.com>
2025-11-04 17:02:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,9 @@ import gdown  # make sure this is imported
 # ---------------------------
 # Internal imports (custom project files)
 # ---------------------------
-from utils.retriever import create_retriever, build_or_load_vectorstore
-from utils.qa_chain import build_qa_chain
-from utils.helpers import format_sources, ensure_model_exists
-from utils.wikipedia_loader import load_wiki_page
+from retriever import create_retriever, build_or_load_vectorstore
+from qa_chain import build_qa_chain
+from wikipedia_loader import load_wiki_page
 
 # ---------------------------
 # Must be FIRST Streamlit command
@@ -205,6 +204,7 @@ if st.session_state.last_retrieved_docs:
 
 st.markdown("---")
 st.caption("Running Phi-2 quantized on CPU. If data is missing, the assistant safely declines to answer.")
+
 
 
 
