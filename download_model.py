1abf8f5894ce83b9f2d25a9758d51f1b0e9e6f5b
Kavya Sri <kavyasrirudrakshala@gmail.com>
2025-11-04 13:21:11 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -5,13 +5,14 @@ import gdown
 url = "https://drive.google.com/uc?export=download&id=1bquBi_ccK4XDsatiHZsucysPUBXzmga6"
 
 
-output = "models/phi-2.Q4_K_M"
+output = "models/phi-2.Q4_K_M.gguf"
 
 os.makedirs(os.path.dirname(output), exist_ok=True)
 
 print("Downloading Llama model file from Google Drive...")
 gdown.download(url, output, quiet=False)
 print(f"✅ Model saved to {output}")
+
 
 
 
